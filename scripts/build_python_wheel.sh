073e75eb34479b670f8728f6eaff28485285280c
Bram Wasti <bwasti@fb.com>
2017-02-14 10:49:12 -0800

    Script to build python wheel file

---
+++ scripts/build_python_wheel.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+##############################################################################
+# This script will build a python wheel file in the folder build/dist
+##############################################################################
+
+CAFFE2_ROOT="$( cd "$(dirname "$0")"/.. ; pwd -P)"
+BUILD_ROOT=$CAFFE2_ROOT/build
+PYTHON_BUILD_ROOT=$CAFFE2_ROOT/build/caffe2
+
+# Build Caffe2
+echo "Building Caffe2"
+cd $BUILD_ROOT
+cmake ..
+make
+
+# Build the wheel from the compiled files
+cd $PYTHON_BUILD_ROOT
+python ../../scripts/setup.py bdist_wheel
+
+echo
+echo
+echo "Python wheel built! Located in build/dist/"
